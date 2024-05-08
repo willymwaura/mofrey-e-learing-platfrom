1019669377c9c2c16376c7b5f640acc84c166932
willymwaura <wilsonmwaura697@gmail.com>
2024-05-09 00:28:54 +0300

    added a new email with its stmp

--- dict/settings.py
+++ dict/settings.py
@@ -144,8 +144,8 @@ SESSION_COOKIE_AGE = 3600 # 30 minutes (30 * 60 seconds)
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = False
-EMAIL_PORT = 26
+#EMAIL_USE_TLS = True
+EMAIL_PORT = 465
 EMAIL_HOST_USER = 'mentorship@mofreyfx.com'
 EMAIL_HOST_PASSWORD="Admin@1993men"
 
