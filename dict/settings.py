b55f0b7eb146fbff51dfec3ba675211d3f6dd1c5
willymwaura <wilsonmwaura697@gmail.com>
2024-05-09 00:08:26 +0300

    added a new email with its stmp

--- dict/settings.py
+++ dict/settings.py
@@ -146,7 +146,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 465
-EMAIL_HOST_USER = 'mentorship@mofreyfxgmail.com'
+EMAIL_HOST_USER = 'mentorship@mofreyfx.com'
 EMAIL_HOST_PASSWORD="Admin@1993men"
 
 
